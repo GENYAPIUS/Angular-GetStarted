70c6a900fa4567500b0c103348e663753493377e
GENYAPIUS <t6589989@gmail.com>
2021-11-17 18:09:16 +0800

    feature: Add product detail

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.css']
+  styleUrls: ['./product-details.component.css'],
 })
 export class ProductDetailsComponent implements OnInit {
-
-  constructor() { }
+  product: Product | undefined;
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-  }
+    // 先從路由取得產品 ID
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
 
+    // 使用路由提供的產品 ID 來取得對應的產品資料
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
 }
